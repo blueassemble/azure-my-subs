127c5beab287500997b5441b5bdd26d4bdaeb302
blueassemble <86643381+blueassemble@users.noreply.github.com>
2024-01-25 05:43:32 +0000

    assign policy

--- policy.tf
+++ policy.tf
@@ -14,9 +14,18 @@ resource "azurerm_user_assigned_identity" "policy" {
   name                = "identity-policy"
   resource_group_name = azurerm_resource_group.policy.name
 }
+resource "azurerm_role_assignment" "policy" {
+  scope = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+  role_definition_name = "Owner"
+  principal_id = azurerm_user_assigned_identity.policy.principal_id
+}
 
 resource "azurerm_subscription_policy_assignment" "deploy_vm_auto_shutdown" {
   name                 = "deploy-vm-autoshutdown-assignment"
   subscription_id          = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
   policy_definition_id = azurerm_policy_definition.deploy_vm_auto_shutdown.id
+  identity {
+    type = "UserAssigned"
+    identity_ids = azurerm_user_assigned_identity.policy.id
+  }
 }

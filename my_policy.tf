ffb9de01f9bf9e539df2c911657f187327581f14
blueassemble <86643381+blueassemble@users.noreply.github.com>
2024-01-26 01:22:00 +0000

    assign initiative

--- my_policy.tf
+++ my_policy.tf
@@ -4,17 +4,6 @@ module "naming_policy" {
   suffix  = ["policy"]
 }
 
-resource "azurerm_subscription_policy_assignment" "deploy_vm_auto_shutdown" {
-  name                 = "Deploy Virtual Machine Autoshutdown Schedule"
-  subscription_id          = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
-  policy_definition_id = azurerm_policy_definition.deploy_vm_auto_shutdown.id
-  location = azurerm_user_assigned_identity.policy.location
-  identity {
-    type = "UserAssigned"
-    identity_ids = [azurerm_user_assigned_identity.policy.id]
-  }
-}
-
 resource "azurerm_policy_set_definition" "my_initiative" {
   name         = "myInitiativeDefinition"
   policy_type  = "Custom"
@@ -22,5 +11,16 @@ resource "azurerm_policy_set_definition" "my_initiative" {
 
   policy_definition_reference {
     policy_definition_id = azurerm_policy_definition.deploy_vm_auto_shutdown.id
+  }
+}
+
+resource "azurerm_subscription_policy_assignment" "my_initiative" {
+  name                 = "My Initiative Assignment"
+  subscription_id          = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+  policy_definition_id = azurerm_policy_set_definition.my_initiative.id
+  location = azurerm_user_assigned_identity.policy.location
+  identity {
+    type = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.policy.id]
   }
 }

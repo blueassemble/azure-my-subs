16b77166cecd128fe4f81f6d91f5abaea7006d33
blueassemble <86643381+blueassemble@users.noreply.github.com>
2024-01-26 00:38:52 +0000

    nochange

--- main.tf
+++ main.tf
@@ -5,3 +5,20 @@
 
 data azurerm_client_config "current" {
 }
+
+
+resource "azurerm_resource_group" "policy" {
+    name = module.naming_policy.resource_group.name
+    location = var.location
+}
+
+resource "azurerm_user_assigned_identity" "policy" {
+  location            = azurerm_resource_group.policy.location
+  name                = "identity-policy"
+  resource_group_name = azurerm_resource_group.policy.name
+}
+resource "azurerm_role_assignment" "policy" {
+  scope = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+  role_definition_name = "Owner"
+  principal_id = azurerm_user_assigned_identity.policy.principal_id
+}
--- my_policy.tf
+++ my_policy.tf
@@ -4,22 +4,6 @@ module "naming_policy" {
   suffix  = ["policy"]
 }
 
-resource "azurerm_resource_group" "policy" {
-    name = module.naming_policy.resource_group.name
-    location = var.location
-}
-
-resource "azurerm_user_assigned_identity" "policy" {
-  location            = azurerm_resource_group.policy.location
-  name                = "identity-policy"
-  resource_group_name = azurerm_resource_group.policy.name
-}
-resource "azurerm_role_assignment" "policy" {
-  scope = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
-  role_definition_name = "Owner"
-  principal_id = azurerm_user_assigned_identity.policy.principal_id
-}
-
 resource "azurerm_subscription_policy_assignment" "deploy_vm_auto_shutdown" {
   name                 = "Deploy Virtual Machine Auto Shutdown Schedule"
   subscription_id          = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"

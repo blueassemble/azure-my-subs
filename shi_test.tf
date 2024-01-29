b9b941913dfcbde3bd4958cb8cd4674372fdebde
blueassemble <86643381+blueassemble@users.noreply.github.com>
2024-01-29 04:59:34 +0000

    deploy mssql vm

--- shi_test.tf
+++ shi_test.tf
@@ -27,6 +27,10 @@ resource "azurerm_subnet" "shi" {
   resource_group_name  = azurerm_resource_group.shi.name
   virtual_network_name = azurerm_resource_group.shi.name
   address_prefixes     = ["10.0.2.0/24"]
+
+  depends_on = [
+    azurerm_virtual_network.shi
+  ]
 }
 
 resource "azurerm_public_ip" "shi" {

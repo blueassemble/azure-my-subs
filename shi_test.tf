1a3e27e86b2e7aea6f724fbf36223c590b489fc7
blueassemble <86643381+blueassemble@users.noreply.github.com>
2024-01-29 23:18:00 +0000

    change os version

--- shi_test.tf
+++ shi_test.tf
@@ -136,7 +136,7 @@ resource "azurerm_windows_virtual_machine" "shi-ir" {
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
-    sku       = "2016-Datacenter"
+    sku       = "2019-Datacenter"
     version   = "latest"
   }
 }

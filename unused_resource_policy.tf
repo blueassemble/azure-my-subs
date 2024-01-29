3253087fe4eab779a60faf0d4cfc7828285d049e
blueassemble <86643381+blueassemble@users.noreply.github.com>
2024-01-29 00:31:50 +0000

    add unused disks/publicips policy

--- unused_resource_policy.tf
+++ unused_resource_policy.tf
@@ -35,3 +35,41 @@ module "audit_unused_serverfarms" {
 locals {
   audit_unused_serverfarms = jsondecode(file("policy_template/audit_unused_serverfarms.json"))
 }
+
+module "audit_unused_disks" {
+  source              = "gettek/policy-as-code/azurerm//modules/definition"
+  file_path           = "../policy_template/audit_cost_optimization/audit_unused_disks.json"
+  
+  policy_name         = (local.audit_unused_disks).properties.displayName
+  display_name        = (local.audit_unused_disks).properties.displayName
+  policy_description  = (local.audit_unused_disks).properties.description
+  policy_category     = (local.audit_unused_disks).properties.metadata.category
+  policy_version      = (local.audit_unused_disks).properties.metadata.version
+
+  policy_rule       = (local.audit_unused_disks).properties.policyRule
+  policy_parameters = (local.audit_unused_disks).properties.parameters
+  policy_metadata   = (local.audit_unused_disks).properties.metadata
+}
+
+locals {
+  audit_unused_disks = jsondecode(file("policy_template/audit_unused_disks.json"))
+}
+
+module "audit_unused_publicips" {
+  source              = "gettek/policy-as-code/azurerm//modules/definition"
+  file_path           = "../policy_template/audit_cost_optimization/audit_unused_publicips.json"
+  
+  policy_name         = (local.audit_unused_publicips).properties.displayName
+  display_name        = (local.audit_unused_publicips).properties.displayName
+  policy_description  = (local.audit_unused_publicips).properties.description
+  policy_category     = (local.audit_unused_publicips).properties.metadata.category
+  policy_version      = (local.audit_unused_publicips).properties.metadata.version
+
+  policy_rule       = (local.audit_unused_publicips).properties.policyRule
+  policy_parameters = (local.audit_unused_publicips).properties.parameters
+  policy_metadata   = (local.audit_unused_publicips).properties.metadata
+}
+
+locals {
+  audit_unused_publicips = jsondecode(file("policy_template/audit_unused_publicips.json"))
+}

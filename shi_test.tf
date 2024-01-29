2125efc65c44aca082a27f8e4c281de389d5e6dc
blueassemble <86643381+blueassemble@users.noreply.github.com>
2024-01-29 05:59:31 +0000

    rename ir vm

--- shi_test.tf
+++ shi_test.tf
@@ -105,7 +105,7 @@ resource "azurerm_public_ip" "shi-ir" {
 }
 
 resource "azurerm_network_interface" "shi-ir" {
-  name                = module.naming_shi.network_interface.name
+  name                = "${module.naming_shi.network_interface.name}-ir"
   location            = azurerm_resource_group.shi.location
   resource_group_name = azurerm_resource_group.shi.name
 

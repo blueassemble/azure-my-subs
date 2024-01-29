05fbce29bdb81825e36e32b3bb27750aaf995ae1
blueassemble <86643381+blueassemble@users.noreply.github.com>
2024-01-29 05:01:36 +0000

    deploy mssql vm

--- shi_test.tf
+++ shi_test.tf
@@ -25,7 +25,7 @@ resource "azurerm_virtual_network" "shi" {
 resource "azurerm_subnet" "shi" {
   name                 = module.naming_shi.subnet.name
   resource_group_name  = azurerm_resource_group.shi.name
-  virtual_network_name = azurerm_resource_group.shi.name
+  virtual_network_name = azurerm_virtual_network.shi.name
   address_prefixes     = ["10.0.2.0/24"]
 
   depends_on = [

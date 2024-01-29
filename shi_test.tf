9da54338ffac9257b84558e7d95cf9cf97565713
blueassemble <86643381+blueassemble@users.noreply.github.com>
2024-01-29 05:53:18 +0000

    deploy ir server

--- shi_test.tf
+++ shi_test.tf
@@ -96,3 +96,47 @@ resource "azurerm_mssql_virtual_machine" "shi" {
     azurerm_windows_virtual_machine.shi
   ]
 }
+
+resource "azurerm_public_ip" "shi-ir" {
+  name                = module.naming_shi.public_ip.name
+  resource_group_name = azurerm_resource_group.shi.name
+  location            = azurerm_resource_group.shi.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "shi-ir" {
+  name                = module.naming_shi.network_interface.name
+  location            = azurerm_resource_group.shi.location
+  resource_group_name = azurerm_resource_group.shi.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.shi.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.shi-ir.id
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "shi-ir" {
+  name                = "${module.naming_shi.virtual_machine.name}-ir"
+  resource_group_name = azurerm_resource_group.shi.name
+  location            = azurerm_resource_group.shi.location
+  size                = "Standard_F2"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [
+    azurerm_network_interface.shi-ir.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}

4d770685d7ae2154afc1e99b5ece8d57b2138d8e
blueassemble <86643381+blueassemble@users.noreply.github.com>
2024-01-29 00:03:37 +0000

    rename policy

--- unused_resource_policy.tf
+++ unused_resource_policy.tf
@@ -2,11 +2,11 @@ module "audit_ahub_definition" {
   source              = "gettek/policy-as-code/azurerm//modules/definition"
   file_path           = "../policy_template/audit_AHUB_for_eligible_vms.json"
   
-  policy_name         = "Custom Name"
-  display_name        = "Custom Display Name"
-  policy_description  = "Custom Description"
-  policy_category     = "Custom Category"
-  policy_version      = "Custom Version"
+  policy_name         = (local.policy_file).properties.displayName
+  display_name        = (local.policy_file).properties.displayName
+  policy_description  = (local.policy_file).properties.description
+  policy_category     = (local.policy_file).properties.metadata.category
+  policy_version      = (local.policy_file).properties.metadata.version
 
   policy_rule       = (local.policy_file).properties.policyRule
   policy_parameters = (local.policy_file).properties.parameters

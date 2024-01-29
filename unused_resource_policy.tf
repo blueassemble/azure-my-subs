0a4a63651b75b048cda95bfaecc223dca86925eb
blueassemble <86643381+blueassemble@users.noreply.github.com>
2024-01-29 00:17:38 +0000

    add unused serverfarms policy

--- unused_resource_policy.tf
+++ unused_resource_policy.tf
@@ -2,17 +2,36 @@ module "audit_ahub_definition" {
   source              = "gettek/policy-as-code/azurerm//modules/definition"
   file_path           = "../policy_template/audit_AHUB_for_eligible_vms.json"
   
-  policy_name         = (local.policy_file).properties.displayName
-  display_name        = (local.policy_file).properties.displayName
-  policy_description  = (local.policy_file).properties.description
-  policy_category     = (local.policy_file).properties.metadata.category
-  policy_version      = (local.policy_file).properties.metadata.version
+  policy_name         = (local.audit_ahub).properties.displayName
+  display_name        = (local.audit_ahub).properties.displayName
+  policy_description  = (local.audit_ahub).properties.description
+  policy_category     = (local.audit_ahub).properties.metadata.category
+  policy_version      = (local.audit_ahub).properties.metadata.version
 
-  policy_rule       = (local.policy_file).properties.policyRule
-  policy_parameters = (local.policy_file).properties.parameters
-  policy_metadata   = (local.policy_file).properties.metadata
+  policy_rule       = (local.audit_ahub).properties.policyRule
+  policy_parameters = (local.audit_ahub).properties.parameters
+  policy_metadata   = (local.audit_ahub).properties.metadata
 }
 
 locals {
-  policy_file = jsondecode(file("policy_template/audit_AHUB_for_eligible_vms.json"))
+  audit_ahub = jsondecode(file("policy_template/audit_AHUB_for_eligible_vms.json"))
+}
+
+module "audit_unused_serverfarms" {
+  source              = "gettek/policy-as-code/azurerm//modules/definition"
+  file_path           = "../policy_template/audit_unused_serverfarms.json"
+  
+  policy_name         = (local.audit_unused_serverfarms).properties.displayName
+  display_name        = (local.audit_unused_serverfarms).properties.displayName
+  policy_description  = (local.audit_unused_serverfarms).properties.description
+  policy_category     = (local.audit_unused_serverfarms).properties.metadata.category
+  policy_version      = (local.audit_unused_serverfarms).properties.metadata.version
+
+  policy_rule       = (local.audit_unused_serverfarms).properties.policyRule
+  policy_parameters = (local.audit_unused_serverfarms).properties.parameters
+  policy_metadata   = (local.audit_unused_serverfarms).properties.metadata
+}
+
+locals {
+  audit_unused_serverfarms = jsondecode(file("policy_template/audit_unused_serverfarms.json"))
 }

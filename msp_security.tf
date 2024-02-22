e4591d642db80942e913092a97f9e5a92e73f86d
blueassemble <86643381+blueassemble@users.noreply.github.com>
2024-02-22 07:46:10 +0000

    delete mssql

--- msp_security.tf
+++ msp_security.tf
@@ -26,24 +26,3 @@ resource "azurerm_subnet" "msp_security" {
     azurerm_virtual_network.msp_security
   ]
 }
-
-# resource "azurerm_mssql_server" "msp_security" {
-#   count                        = 2
-#   name                         = "${module.msp_security.mssql_server.name}-${count.index}"
-#   resource_group_name          = azurerm_resource_group.msp_security.name
-#   location                     = azurerm_resource_group.msp_security.location
-#   version                      = "12.0"
-#   administrator_login          = var.admin_username
-#   administrator_login_password = var.admin_password
-# }
-
-# resource "azurerm_mssql_database" "msp_security" {
-#   count          = 2
-#   name           = "${module.msp_security.mssql_database.name}-${count.index}"
-#   server_id      = azurerm_mssql_server.msp_security[count.index].id
-#   collation      = "SQL_Latin1_General_CP1_CI_AS"
-#   license_type   = "LicenseIncluded"
-#   sku_name       = "Basic"
-#   enclave_type   = "VBS"
-# }
-
--- msp_security_mssql.tf
+++ msp_security_mssql.tf
@@ -1,20 +1,20 @@
-resource "azurerm_mssql_server" "msp_security" {
-  count                        = 2
-  name                         = "${module.msp_security.mssql_server.name}-${count.index}"
-  resource_group_name          = azurerm_resource_group.msp_security.name
-  location                     = azurerm_resource_group.msp_security.location
-  version                      = "12.0"
-  administrator_login          = var.admin_username
-  administrator_login_password = var.admin_password
-}
+# resource "azurerm_mssql_server" "msp_security" {
+#   count                        = 2
+#   name                         = "${module.msp_security.mssql_server.name}-${count.index}"
+#   resource_group_name          = azurerm_resource_group.msp_security.name
+#   location                     = azurerm_resource_group.msp_security.location
+#   version                      = "12.0"
+#   administrator_login          = var.admin_username
+#   administrator_login_password = var.admin_password
+# }
 
-resource "azurerm_mssql_database" "msp_security" {
-  count          = 2
-  name           = "${module.msp_security.mssql_database.name}-${count.index}"
-  server_id      = azurerm_mssql_server.msp_security[count.index].id
-  collation      = "SQL_Latin1_General_CP1_CI_AS"
-  license_type   = "LicenseIncluded"
-  sku_name       = "Basic"
-  enclave_type   = "VBS"
-}
+# resource "azurerm_mssql_database" "msp_security" {
+#   count          = 2
+#   name           = "${module.msp_security.mssql_database.name}-${count.index}"
+#   server_id      = azurerm_mssql_server.msp_security[count.index].id
+#   collation      = "SQL_Latin1_General_CP1_CI_AS"
+#   license_type   = "LicenseIncluded"
+#   sku_name       = "Basic"
+#   enclave_type   = "VBS"
+# }
 

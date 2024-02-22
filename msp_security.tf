030ae7562120e334aab4f6d734988ed86b59e57b
blueassemble <86643381+blueassemble@users.noreply.github.com>
2024-02-22 23:20:27 +0000

    deploy mssql

--- msp_security.tf
+++ msp_security.tf
@@ -20,7 +20,7 @@ resource "azurerm_subnet" "msp_security" {
   name                 = module.msp_security.subnet.name
   resource_group_name  = azurerm_resource_group.msp_security.name
   virtual_network_name = azurerm_virtual_network.msp_security.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = ["10.0.0.0/24"]
 
   depends_on = [
     azurerm_virtual_network.msp_security

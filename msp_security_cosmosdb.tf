101e7bd1c86d0bb99d4e7e726acee4d057c20116
blueassemble <86643381+blueassemble@users.noreply.github.com>
2024-02-27 04:43:11 +0000

    deploy cosmosdb account

---
+++ msp_security_cosmosdb.tf
@@ -0,0 +1,19 @@
+resource "azurerm_cosmosdb_account" "msp_security" {
+  count               = 2
+  name                = "${module.msp_security.cosmosdb_account.name}-${count.index}"
+  location            = azurerm_resource_group.msp_security.location
+  resource_group_name = azurerm_resource_group.msp_security.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  consistency_policy {
+    consistency_level       = "Session"
+    max_interval_in_seconds = 5
+    max_staleness_prefix    = 100
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.msp_security.location
+    failover_priority = 0
+  }
+}

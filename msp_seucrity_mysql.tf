dbbae4d2b2e00dc3ebbafe64e5bda7f033a25490
blueassemble <86643381+blueassemble@users.noreply.github.com>
2024-03-26 05:59:56 +0000

    deploy cosmosdb

--- msp_seucrity_mysql.tf
+++ msp_seucrity_mysql.tf
@@ -19,3 +19,14 @@
 #   collation           = "SQL_Latin1_General_CP1_CI_AS"
 #   charset             = "utf8"
 # }
+
+# resource "azurerm_mysql_flexible_server" "mysql_flexible" {
+#   name                   = "cyanmysqlflex01"
+#   resource_group_name    = azurerm_resource_group.msp_security.name
+#   location               = azurerm_resource_group.msp_security.location
+#   administrator_login    = var.admin_username
+#   administrator_password = var.admin_password
+#   backup_retention_days  = 7
+#   sku_name               = "GP_Standard_D2ds_v4"
+
+# }
